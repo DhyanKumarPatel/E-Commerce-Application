39328d2aa5c2ca9eac059149699f5da6628dc2c1
dhyan <dhyanp92@gmail.com>
2026-08-11 00:34:47 -0400

    api services created, @GET initiated to fetch all the categories from the API. Category Adapter updated. View model created

--- app/src/main/java/com/code4galaxy/e_commerceapp/model/Category.kt
+++
@@ -1,6 +0,0 @@
-package com.code4galaxy.e_commerceapp.model
-
-data class Category(
-    val image: Int,
-    val name: String
-)
--- app/src/main/java/com/code4galaxy/e_commerceapp/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/code4galaxy/e_commerceapp/view/fragments/HomeFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.code4galaxy.e_commerceapp.model.Category
-import com.code4galaxy.e_commerceapp.R
 import com.code4galaxy.e_commerceapp.databinding.FragmentHomeBinding
+import com.code4galaxy.e_commerceapp.network.RetrofitClient
+import com.code4galaxy.e_commerceapp.repository.CategoryRepositoryImpl
+import com.code4galaxy.e_commerceapp.utils.UIState
+import com.code4galaxy.e_commerceapp.view.adapters.CategoryAdapter
+import com.code4galaxy.e_commerceapp.viewModel.CategoryViewModel
+import com.code4galaxy.e_commerceapp.viewModel.CategoryViewModelFactory
 
 class HomeFragment: Fragment() {
 
 private lateinit var binding: FragmentHomeBinding
+
+private lateinit var viewModel: CategoryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,53 +33,63 @@ private lateinit var binding: FragmentHomeBinding
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         setUpRecyclerView()
+        setUpViewModel()
+        setUpObserver()
+        viewModel.getCategories()
+    }
+
+    private fun setUpObserver() {
+        viewModel.categoryState.observe(viewLifecycleOwner){state ->
+            when(state){
+                is UIState.Loading -> {
+
+                    //  progress bar
+                }
+
+                is UIState.Success-> {
+
+                    val response = state.data
+
+                    binding.rvCategories.adapter =
+                        CategoryAdapter(response.categories) { category ->
+
+                            Toast.makeText(
+                                requireContext(),
+                                category.category_name,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+
+                is UIState.Error -> {
+
+                    Toast.makeText(
+                        requireContext(),
+                        state.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {}
+            }
+        }
+    }
+
+    private fun setUpViewModel() {
+        val repository = CategoryRepositoryImpl(RetrofitClient.apiServices)
+
+        val factory = CategoryViewModelFactory(repository)
+
+        viewModel = ViewModelProvider(this, factory)[CategoryViewModel::class.java]
     }
 
 
-
     private fun setUpRecyclerView() {
-        val categories = listOf(
-            Category(
-                image = R.drawable.smartpone,
-                name = "Smart Phones"
-            ),
-            Category(
-                image = R.drawable.smartpone,
-                name = "Laptops"
-            ),
-            Category(
-                image = R.drawable.smartpone,
-                name = "Mens Wear"
-            ),
-            Category(
-                image = R.drawable.smartpone,
-                name = "Women’s Wear"
-            ),
-            Category(
-                image = R.drawable.smartpone,
-                name = "Kids Wear"
-            ),
-            Category(
-                image = R.drawable.smartpone,
-                name = "Grocery"
-            )
-        )
 
         binding.rvCategories.layoutManager = GridLayoutManager(requireContext(), 2)
-
-        binding.rvCategories.adapter = CategoryAdapter(categories) { category ->
-
-            when (category.name) {
-                "Smart Phones" -> {
-                    parentFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainer, SmartPhoneFragment())
-                        .addToBackStack(null)
-                        .commit()
-                }
-            }
-        }
-
 
     }
 
